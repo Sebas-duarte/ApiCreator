217deb9c62db5301f762cfae2eaa5bf3b1e19789
Sebas-duarte <163492596+Sebas-duarte@users.noreply.github.com>
2025-10-17 01:35:27 +0000

    cambios guardados

--- config/jwt.py
+++ config/jwt.py
@@ -1,7 +1,8 @@
-# Configuración de JWT
-import os
-JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+from datetime import timedelta
+
+
+JWT_SECRET_KEY = "SML_2025"
 JWT_TOKEN_LOCATION = ["headers"]
-JWT_ACCESS_TOKEN_EXPIRES = 3600
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
 JWT_HEADER_NAME = "Authorization"
 JWT_HEADER_TYPE = "Bearer"
